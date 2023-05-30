5cccf1b7ecff7b69c8f6e414e96aa01b085e4ebb
Mateus Holanda <mateus23ita@gmail.com>
2023-05-30 00:57:56 -0300

    Fix bug in StarCanvas.tsx ref type

--- src/components/StarsCanvas.tsx
+++ src/components/StarsCanvas.tsx
@@ -5,28 +5,31 @@ import { PointMaterial, Points, Preload } from '@react-three/drei'
 import { Canvas, useFrame } from '@react-three/fiber'
 import * as random from 'maath/random/dist/maath-random.esm'
 import { Suspense, useRef } from 'react'
+import { Mesh } from 'three'
 
 function Stars() {
-  const ref = useRef(null)
+  const meshRef = useRef<Mesh>(null!)
 
   const sphere = random.inSphere(new Float32Array(90000), { radius: 5 })
 
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 40
-    ref.current.rotation.y -= delta / 60
+    meshRef.current.rotation.x -= delta / 40
+    meshRef.current.rotation.y -= delta / 60
   })
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled>
-        <PointMaterial
-          transparent
-          color="#f272c8"
-          size={0.002}
-          sizeAttenuation={true}
-          depthWrite={false}
-        />
-      </Points>
+      <mesh ref={meshRef}>
+        <Points positions={sphere} stride={3} frustumCulled>
+          <PointMaterial
+            transparent
+            color="#f272c8"
+            size={0.002}
+            sizeAttenuation={true}
+            depthWrite={false}
+          />
+        </Points>
+      </mesh>
     </group>
   )
 }
